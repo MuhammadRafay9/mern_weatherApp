52d2e2ff9abbcaa20662227c8fd7d003d4730b37
AhmedRaza <asimn6062@gmail.com>
2022-12-05 21:28:14 +0500

    final test

--- server.mjs
+++ server.mjs
@@ -4,33 +4,48 @@ import cors from 'cors';
 import request from 'request';
 
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 
+app.get('/abc', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
+})
+
 app.get('/weather/:cityName', (req, res) => {
   
-    const requestOptions = {
-        url: `https://api.openweathermap.org/data/2.5/forecast?q=${req.params.cityName}&appid=184ae4d8dfeddc69b4b96699c3d202ab&units=metric`,
-        method: 'GET',
-        json: {},
-        qs: {
-          offset: 20
-        }
-      };
-      request(requestOptions, (err, response, body) => {
-        if (err) {
-          res.send(err);
-        } else if (response.statusCode === 200) {
-          res.send(body);
-        } else {
-          res.send(response.statusCode);
-        }
-      });
+  const requestOptions = {
+      url: `https://api.openweathermap.org/data/2.5/forecast?q=${req.params.cityName}&appid=184ae4d8dfeddc69b4b96699c3d202ab&units=metric`,
+      method: 'GET',
+      json: {},
+      qs: {
+        offset: 20
+      }
+    };
+    request(requestOptions, (err, response, body) => {
+      if (err) {
+        res.send(err);
+      } else if (response.statusCode === 200) {
+        res.send(body);
+      } else {
+        res.send(response.statusCode);
+      }
+    });
 
 })
 
 
+app.get('/time', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
+})
+
 const __dirname = path.resolve();
 app.use('/', express.static(path.join(__dirname, './web/build')))
 app.use('*', express.static(path.join(__dirname, './web/build')))
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
